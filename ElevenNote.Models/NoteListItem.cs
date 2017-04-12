a3b3845cae6610da0bc385b92d74cb4676f6c0aa
Kenn Pascascio <kenn370@gmail.com>
2017-04-12 15:42:44 -0400

    added the create view and also the note create

--- ElevenNote.Models/NoteListItem.cs
+++ ElevenNote.Models/NoteListItem.cs
@@ -25,5 +25,7 @@ namespace ElevenNote.Models
         [Display(Name = "Created")]
         public DateTimeOffset CreatedUtc { get; set; }
         //we need to add in the reference but it's present in the MVC, VS is smarrt enough to help us out wuth that 
+
+        public override string ToString() => $"[{NoteId}] title";
     }
 }
--- ElevenNote.Web/Controllers/NoteController.cs
+++ ElevenNote.Web/Controllers/NoteController.cs
@@ -17,8 +17,30 @@ namespace ElevenNote.Web.Controllers
         {
             //bringing in the model from the model layer
             //Notice that we passed in the array
+            // think of how we use string[] 
+            // NoteListItem is a data type and we want to pass in an empty array of zero to have something to iterate over.
             var model = new NoteListItem[0];
         
+            return View(model);
+        }
+
+        //We aren't giving any data, we are going to be creating data with this
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            //while blank we are going to set a break point to check out the data that is being 
+            //passed. 
+            if (ModelState.IsValid)
+            {
+
+            }
             return View(model);
         }
     }
