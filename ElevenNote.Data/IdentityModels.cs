b4c31008b81fedc8e76ea0f856f9886dfb1744c5
Kenn Pascascio <kenn370@gmail.com>
2017-04-12 11:10:50 -0400

    fixed assembly errors build

---
+++ ElevenNote.Data/IdentityModels.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+/*
+ * 
+ * Moved this file from ElevenNote.Web, We have to change the namespace to the one for this project
+ * which is ElevenNote.Data
+ * Alot of this are broken right when you move.
+ * the first change is in line 1 with the entity using, so we need to bring in a Nuget package to
+ * this file add in the Microsoft.AspNet.Identity.EntityFramework and select this(ElevenNote.Data) project. 
+ * 
+ * All of the red lines should have gone away.
+ * 
+ */
+namespace ElevenNote.Data
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
