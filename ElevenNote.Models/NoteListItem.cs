ad2a8072ecb0be896c0478c4ec0fb56908e71376
Kenn Pascascio <kenn370@gmail.com>
2017-04-12 15:07:36 -0400

    added the note functionality to the Web side

---
+++ ElevenNote.Models/NoteListItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteListItem
+    {
+        /*
+         *  Make you're class public so that it can be used throughout the application 
+         * 
+         * 
+         * poco == plain old clr object
+         */
+
+        public int NoteId { get; set; }
+        //We need to tell the webside which note we want to edit, that's why we build a NoteId
+
+
+        public string Title { get; set; }
+    
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+        //we need to add in the reference but it's present in the MVC, VS is smarrt enough to help us out wuth that 
+    }
+}
---
+++ ElevenNote.Web/Controllers/NoteController.cs
@@ -0,0 +1,25 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.Web.Controllers
+{
+    //we want to be able to only have a logged in user see these pages 
+    // let's let MVC framework do this work with this annotation
+    // this says when you get a request make sure that the user is authenticated, if not direct ot login page
+    [Authorize]
+    public class NoteController : Controller
+    {
+        public ActionResult Index()
+        {
+            //bringing in the model from the model layer
+            //Notice that we passed in the array
+            var model = new NoteListItem[0];
+        
+            return View(model);
+        }
+    }
+}
