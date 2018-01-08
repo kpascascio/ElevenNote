55ae859d4052c5f6abfad65f97fc72579d97f663
Kenn Pascascio <kenn370@gmail.com>
2018-01-08 12:01:28 -0500

    Added manage users service, and controller that retrieves all users

---
+++ ElevenNote.Services/ManageUsers.cs
@@ -0,0 +1,37 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class ManageUsers
+    {
+
+        public ManageUsers()
+        {
+
+        }
+
+        public IEnumerable<UsersListItem> GetUsers()
+        {   
+            using(var context = new ApplicationDbContext())
+            {
+                var query = context.Users.Select(
+                    e => new UsersListItem
+                    {
+                        UserId = e.Id,
+                        Email = e.Email,
+                        UserName = e.UserName
+                    }
+                );
+
+                return query.ToArray();
+            }
+
+        }
+    }
+}
